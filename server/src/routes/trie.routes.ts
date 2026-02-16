f84b955a58a289a1d2e7a42c233348f6f185951f
Oliver Metz <softwaredevdad@gmail.com>
2026-02-16 16:02:34 +0100

    Update backend route.
    - Update the backend trie route as vercel issue with caching and not
    returning our auto suggestions?

--- server/src/routes/trie.routes.ts
+++ server/src/routes/trie.routes.ts
@@ -10,6 +10,12 @@ router.get(
   catchAsync(async (req, res, next) => {
     const { q } = req.query;
 
+    /*
+			304 was likely caused by Vercel's default caching. I added "no-store, 
+			no-cache, must-revalidate" headers to prevent caching for autocomplete.
+		*/
+    res.set("Cache-Control", "no-store, no-cache, must-revalidate");
+
     if (!q || typeof q !== "string" || q.length === 0) {
       return sendError(res, "Query parameter 'q' is required.", 400);
     }
